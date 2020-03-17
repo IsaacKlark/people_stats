73a35e68b050e8ccc2f6f5ffccf0288eca9fee1e
IsaacKlark <isaackeklarke@gmail.com>
2020-03-17 13:02:43 +0200

    Updates

---
+++ precache-manifest.3b360bf9b87f4803de8cb53f9c4c2ac7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3bf73c1ee922b72704496b927c92e3",
+    "url": "/people_stats/index.html"
+  },
+  {
+    "revision": "40f4db7185ef8b927608",
+    "url": "/people_stats/static/css/main.3762400f.chunk.css"
+  },
+  {
+    "revision": "a1d4941eed4b103184af",
+    "url": "/people_stats/static/js/2.1907d6eb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/people_stats/static/js/2.1907d6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f4db7185ef8b927608",
+    "url": "/people_stats/static/js/main.fc856566.chunk.js"
+  },
+  {
+    "revision": "3fd51fe7c1bbadb20e05",
+    "url": "/people_stats/static/js/runtime-main.c82738b9.js"
+  },
+  {
+    "revision": "36e9d72635a1427e773344f9771034f6",
+    "url": "/people_stats/static/media/arrow-back-active.36e9d726.svg"
+  },
+  {
+    "revision": "c79d99ae70452536eba98dbd940f4744",
+    "url": "/people_stats/static/media/arrow-back.c79d99ae.svg"
+  },
+  {
+    "revision": "466d7bb0d7185e926754988e14965945",
+    "url": "/people_stats/static/media/arrow-forward-inactive.466d7bb0.svg"
+  },
+  {
+    "revision": "5c8b866846f12314c93d522add574e6d",
+    "url": "/people_stats/static/media/arrow-forward.5c8b8668.svg"
+  },
+  {
+    "revision": "e57b90e1caa9158d4f9451ab9b5a8758",
+    "url": "/people_stats/static/media/basic.e57b90e1.svg"
+  },
+  {
+    "revision": "75756ded887b95245cdea3b1f36270b6",
+    "url": "/people_stats/static/media/clean-design.75756ded.svg"
+  },
+  {
+    "revision": "459e820295cc2b2239d808f81faeb1d1",
+    "url": "/people_stats/static/media/headr-back.459e8202.svg"
+  },
+  {
+    "revision": "ace893a34f8f74ffbbb7d00b28cf850d",
+    "url": "/people_stats/static/media/leptop.ace893a3.png"
+  },
+  {
+    "revision": "1134cbade50cc7fd8a1ae38dd0a3f2ca",
+    "url": "/people_stats/static/media/line.1134cbad.svg"
+  },
+  {
+    "revision": "a1d69187b7afe2d580cde08e7fbf6e29",
+    "url": "/people_stats/static/media/phone.a1d69187.png"
+  },
+  {
+    "revision": "1ef5a55fd8848cc02592dbebe4a30018",
+    "url": "/people_stats/static/media/retina-ready.1ef5a55f.svg"
+  },
+  {
+    "revision": "ad75cab07ac4fa45eb60170e0a42ca85",
+    "url": "/people_stats/static/media/secure-data.ad75cab0.svg"
+  },
+  {
+    "revision": "a8fb03922fda3637c73baa0bf2b0b410",
+    "url": "/people_stats/static/media/spot.a8fb0392.svg"
+  },
+  {
+    "revision": "b56d5a060fcd988fe91301f25aa5d1a0",
+    "url": "/people_stats/static/media/standard.b56d5a06.svg"
+  },
+  {
+    "revision": "7bafa25cecbdb7255121c22271160c96",
+    "url": "/people_stats/static/media/unlimited.7bafa25c.svg"
+  },
+  {
+    "revision": "9368d9499ae5c8d9f31d74372003b809",
+    "url": "/people_stats/static/media/waves.9368d949.svg"
+  }
+]);
